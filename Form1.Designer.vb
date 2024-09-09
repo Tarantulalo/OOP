fbdef041a5ae9b8adca538863883b6224087aaec
Tarantulalo <alisasisian@gmail.com>
2024-09-09 11:42:39 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Label4 = New Label()
+        TextBox6 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.ControlDark
+        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(372, 29)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(281, 41)
+        Label1.TabIndex = 0
+        Label1.Text = "STUDENT PROFILE"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = SystemColors.ControlDark
+        Label2.Font = New Font("Segoe UI Semibold", 10.8F, FontStyle.Bold)
+        Label2.Location = New Point(259, 129)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(65, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Name:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = SystemColors.ControlDark
+        Label3.Font = New Font("Segoe UI Semibold", 10.8F, FontStyle.Bold)
+        Label3.Location = New Point(241, 189)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(83, 25)
+        Label3.TabIndex = 2
+        Label3.Text = "Address:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.BackColor = SystemColors.ControlDark
+        Label5.Font = New Font("Segoe UI Semibold", 10.8F, FontStyle.Bold)
+        Label5.Location = New Point(226, 305)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(98, 25)
+        Label5.TabIndex = 4
+        Label5.Text = "Year Level:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.BackColor = SystemColors.ControlDark
+        Label6.Font = New Font("Segoe UI Semibold", 10.8F, FontStyle.Bold)
+        Label6.Location = New Point(250, 246)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(74, 25)
+        Label6.TabIndex = 5
+        Label6.Text = "Course:"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.BackColor = SystemColors.ControlDark
+        Label7.Font = New Font("Segoe UI Semibold", 10.8F, FontStyle.Bold)
+        Label7.Location = New Point(212, 367)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(112, 25)
+        Label7.TabIndex = 6
+        Label7.Text = "Term Grade:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(337, 130)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(281, 27)
+        TextBox1.TabIndex = 7
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(337, 247)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(281, 27)
+        TextBox2.TabIndex = 8
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(337, 189)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(281, 27)
+        TextBox3.TabIndex = 9
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(337, 368)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(281, 27)
+        TextBox4.TabIndex = 10
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(337, 306)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(281, 27)
+        TextBox5.TabIndex = 11
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ControlDark
+        Button1.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        Button1.ForeColor = SystemColors.ActiveCaptionText
+        Button1.Location = New Point(909, 227)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(92, 28)
+        Button1.TabIndex = 12
+        Button1.Text = "CLEAR"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.ControlDark
+        Button2.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        Button2.ForeColor = SystemColors.ActiveCaptionText
+        Button2.Location = New Point(909, 304)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(92, 28)
+        Button2.TabIndex = 13
+        Button2.Text = "EXIT"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.ControlDark
+        Button3.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        Button3.ForeColor = SystemColors.ActiveCaptionText
+        Button3.Location = New Point(909, 154)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(92, 28)
+        Button3.TabIndex = 14
+        Button3.Text = "DISPLAY"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = SystemColors.ControlDark
+        Label4.Font = New Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(697, 202)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(124, 25)
+        Label4.TabIndex = 15
+        Label4.Text = "FINAL GRADE"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(697, 247)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(125, 27)
+        TextBox6.TabIndex = 16
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.HotTrack
+        ClientSize = New Size(1038, 482)
+        Controls.Add(TextBox6)
+        Controls.Add(Label4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox6 As TextBox
+
+End Class
